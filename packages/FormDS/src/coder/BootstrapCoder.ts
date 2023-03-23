2a4d265c496343258d2139a0275ec8dfaa21771a
prasanthmj <prasanthmj@gmail.com>
2023-03-23 17:28:34 +0530

    Added width class calculations to CSS frameworks

--- packages/FormDS/src/coder/BootstrapCoder.ts
+++ packages/FormDS/src/coder/BootstrapCoder.ts
@@ -1,22 +1,48 @@
 import {CSSFramework} from "./CSSFramework"
 
 export class BootstrapCoder implements CSSFramework{
+    private numColumns:number=2;
+    private breakPoint:string="md";
+
+    setNumColumns(n:number){
+        this.numColumns = n
+    }
+
     inputContainerClasses(width:number):string[]{
         let colx = ''
-        if (width <= 25) {
-            colx = 'col-md-3'
+        
+        let colSliceCount =1;
+        let widthPerc =100
+
+        if(this.numColumns == 4){
+            colSliceCount=3
+            widthPerc = 25
+        }else if(this.numColumns == 3){
+            colSliceCount=4
+            widthPerc = 33
+        }else if(this.numColumns == 2){
+            colSliceCount=6
+            widthPerc = 50
+        }else if(this.numColumns == 1){
+            colSliceCount=12
+            widthPerc = 100
         }
-        else if (width <= 50) {
-            colx = 'col-md-6'
-        }
-        else if (width <= 75) {
-            colx = 'col-md-9'
+
+        if (width) {
+            colx = 'col-'+this.breakPoint+'-12';
+            
+            for(let w=1; w*widthPerc < 100; w++){
+                if(width <= w*widthPerc){
+                    colx = 'col-'+this.breakPoint+'-'+( w * colSliceCount);
+                    break;
+                }
+            }
         }
         else {
-            colx = 'col-12'
+            colx = 'col';
         }
-        
         return [colx];
+
     }
     labelClasses():string[]{
         return ["form-label"]
--- packages/FormDS/src/coder/TailwindCoder.ts
+++ packages/FormDS/src/coder/TailwindCoder.ts
@@ -1,9 +1,39 @@
 import {CSSFramework} from "./CSSFramework"
 
 export class TailWindCoder implements CSSFramework{
+    private numColumns:number=2;
+    private breakPoint:string="md";
+
+    setNumColumns(n:number){
+        this.numColumns = n
+    }
 
     inputContainerClasses(width:number){
-        let colx = ''
+
+        let colx="";
+
+        if(this.numColumns == 2){
+            if(width <= 50){
+                colx = "w-1/2";
+            }
+        } else if(this.numColumns == 3){
+            if(width <= 33){
+                colx = "w-1/3";
+            } else if(width <= 66){
+                colx = "w-2/3";
+            }
+        } else if(this.numColumns == 4){
+            if (width <= 25) {
+                colx = 'w-1/4'
+            }
+            else if (width <= 50) {
+                colx = 'w-1/2'
+            }
+            else if (width <= 75) {
+                colx = 'w-3/4'
+            }
+        }
+        /*let colx = ''
         if (width <= 25) {
             colx = 'w-1/4'
         }
@@ -15,8 +45,14 @@ export class TailWindCoder implements CSSFramework{
         }
         else {
             colx = 'w-full'
+        }*/
+        const ret =["w-full"]
+        if(colx){
+            colx = this.breakPoint+':'+colx
+            ret.push(colx)
         }
-        return [colx];
+        
+        return ret;
     }
 
     labelClasses():string[]{
